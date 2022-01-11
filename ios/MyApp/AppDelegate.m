2e36f1f00b679c2b59412d174512fd7c65744fb7
Stephen Mooney <61681788+smooney211993@users.noreply.github.com>
2022-01-11 11:15:47 +1100

    Configured firebase in an M1 dev environment
    Configured Firebase in an m1 dev environment.

--- ios/MyApp/AppDelegate.m
+++ ios/MyApp/AppDelegate.m
@@ -3,6 +3,8 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
+
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -31,6 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
+  [FIRApp configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"MyApp"
